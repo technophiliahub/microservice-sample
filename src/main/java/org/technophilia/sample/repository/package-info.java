01197553e02062e3b6daf51de2537558233e4c5e
gajendran <gajendran.g@ishafoundation.org>
2020-02-12 12:10:48 +0530

    Initial application generated by JHipster-5.7.1

---
+++ src/main/java/org/technophilia/sample/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package org.technophilia.sample.repository;
---
+++ src/main/java/org/technophilia/sample/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.technophilia.sample.web.rest.vm;
